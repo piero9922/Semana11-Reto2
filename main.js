eb5a744833c835a7799c3086a5fd12b6ff129d78
Piero Montalvo <piero.montalvo9922@gmail.com>
2020-06-13 14:12:30 -0500

    Reto2

---
+++ main.js
@@ -0,0 +1,35 @@
+const form =document.getElementById('formulario')
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
+
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+
+    const contenedorTarea = document.createElement('div')
+    contenedorTarea.classList.add('tarea')
+    
+    console.log(contenedorTarea)
+    
+    const tarea = document.createElement('div')
+    const valor1 = input1.value
+
+    tarea.innerHTML = valor1
+    
+
+    const tarea2 = document.createElement('div')
+    const valor2 = input2.value
+
+    tarea2.innerHTML = valor2;
+
+
+    contenedorTarea.appendChild(tarea)
+    contenedorTarea.appendChild(tarea2)
+
+    const pizarra = document.getElementById('pizarra')
+    pizarra.appendChild(contenedorTarea)
+
+})
+
+
+
